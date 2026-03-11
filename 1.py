314269e7317a641f6085908b798e45ea8dd034a0
smalltur <156491562+smalltur@users.noreply.github.com>
2026-03-11 21:13:51 +0800

    1
    1

--- 1.py
+++ 1.py
@@ -4,11 +4,17 @@ import torch.nn.functional as F
 import math
 print("torch version:", torch.__version__)
 
-class Tokenembedding(nn.Module):
+#将输入的词语转换为对应的词向量
+class TokenEmbedding(nn.Module):
     def __init__(self, vocab_size, d_model):
-        super().__init__()
-        self.d_model = d_model
-        self.embedding = nn.Embedding(vocab_size, d_model)
+        super(TokenEmbedding, self).__init__(vocab_size, d_model, padding_idx=1)
 
-    def forward(self, x):
-        return self.embedding(x)
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=5000, device = torch.device('cpu')):
+        super(PositionalEncoding, self).__init__()
+        self.encoding = torch.zeros(max_len, d_model).to(device)
+        self.encoding.requires_grad = False
+        pos = torch.arange(0, max_len, device=device).float().unsqueeze(1)
+        _2i = torch.arange(0, d_model, step=2, device=device).float()
+        self.encoding[:, 0::2] = torch.sin(pos / (10000 ** (_2i / d_model)))
+        
