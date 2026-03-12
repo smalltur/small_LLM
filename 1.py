ef98e3b1e04016afa35c279ae6196755b1a976a2
smalltur <156491562+smalltur@users.noreply.github.com>
2026-03-12 19:15:07 +0800

    1
    1

--- 1.py
+++ 1.py
@@ -17,4 +17,15 @@ class PositionalEncoding(nn.Module):
         pos = torch.arange(0, max_len, device=device).float().unsqueeze(1)
         _2i = torch.arange(0, d_model, step=2, device=device).float()
         self.encoding[:, 0::2] = torch.sin(pos / (10000 ** (_2i / d_model)))
-        
+        self.encoding[:, 1::2] = torch.cos(pos / (10000 ** (_2i / d_model)))
+    def forward(self, x):
+        batch_size, seq_len = x.size()# x的维度是(batch_size, seq_len)
+        return self.encoding[:seq_len, :].unsqueeze(0).expand(batch_size, -1, -1)
+    
+class TransformerEmbedding(nn.Module):
+    def __init__(self, vocab_size, d_model, max_len=5000, drop_prob=0.1, device = torch.device('cpu')):
+        super(TransformerEmbedding, self).__init__()
+        self.tok_emb = TokenEmbedding(vocab_size, d_model)
+        self.pos_emb = PositionalEncoding(d_model, max_len, device)
+    def forward(self, x):
+        return self.tok_emb(x) + self.pos_emb(x)
